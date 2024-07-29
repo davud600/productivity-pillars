5fcdfbedf3bbc367b5e7f5226c928f02af11bb52
davud600 <davudgorqi@gmail.com>
2024-07-29 18:48:40 +0200

    Feat: admin user

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -7,7 +7,15 @@ type EnvVariables = {
   CORS_ORIGIN: string
   JWT_SECRET: string
   JWT_EXPIRATION: string
+  ADMIN_USERNAME: string
+  ADMIN_PASSWORD: string
 }
 
-export const { PORT, CORS_ORIGIN, JWT_SECRET, JWT_EXPIRATION } =
-  process.env as EnvVariables
+export const {
+  PORT,
+  CORS_ORIGIN,
+  JWT_SECRET,
+  JWT_EXPIRATION,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
+} = process.env as EnvVariables
